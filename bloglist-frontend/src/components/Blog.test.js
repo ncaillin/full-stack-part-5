ccc31e1967c521de65315987aeb9e57770dde33d
ncaillin <nugentcaillin@gmail.com>
2022-12-15 07:23:31 +1100

    5.14

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -3,31 +3,48 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+describe('testing blog page', () => {
+  let container
+  beforeEach(() => {
+    const user = {
+      username: 'testUser',
+      name: 'testy',
+    }
+    const blog = {
+      title: 'testTitle',
+      author: 'Foo',
+      url: 'bar',
+      likes: 12,
+      user
+    }
+    const setBlogs = jest.fn()
+    const blogs = []
 
-test('renders title and author, but not url or likes by default', () => {
+    container = render(
+      <Blog blog={blog} user={user} setBlogs={setBlogs} blogs={blogs} />
+    ).container
+  })
+
+  test('renders title and author, but not url or likes by default', () => {
   
-  const user = {
-    username: 'testUser',
-    name: 'testy',
-  }
-  const blog = {
-    title: 'testTitle',
-    author: 'Foo',
-    url: 'bar',
-    likes: 12,
-    user
-  }
-  const setBlogs = jest.fn()
-  const blogs = []
+    
+    const title = screen.getByText('testTitle')
+    const author = screen.getByText('Foo')
+    expect(title).toBeDefined()
+    expect(author).toBeDefined()
+    const div = container.querySelector('.TogglableContent')
+    expect(div).toHaveStyle('display: none')
+    
+  })
+  test('url and likes shown after click', async () => {
+    const div = container.querySelector('.TogglableContent')
+    expect(div).toHaveStyle('display: none')
 
-  const { container } = render(
-    <Blog blog={blog} user={user} setBlogs={setBlogs} blogs={blogs} />
-  )
-  const title = screen.getByText('testTitle')
-  const author = screen.getByText('Foo')
-  expect(title).toBeDefined()
-  expect(author).toBeDefined()
-  const div = container.querySelector('.TogglableContent')
-  expect(div).toHaveStyle('display: none')
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
 
+    screen.getByText('url: bar')
+    screen.getByText('likes: 12')
+  })
 })
